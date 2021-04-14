0ba715b572d42f4b2df7e68aafc6cfdd9f0b8203
Mattskywalker <mateusejesus@hotmail.com.br>
2021-04-14 18:34:26 -0300

    teste ToastAndroid

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import {styleApps} from './styles/StyleApp/StyleApp'
 
 export default function App() {
+
+ const showToast = () => {
+   ToastAndroid.show("Message",ToastAndroid.SHORT);
+ } 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styleApps.container}>
+
+      <TouchableOpacity style = {styleApps.button} onPress={()=>{showToast()}}>
+        <Text>Press</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
